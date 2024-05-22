d8953f3e45c10ef97c31f8b7875ee70559f6cad6
LumWenKang <lumlaoren@gmail.com>
2024-05-23 00:38:19 +0800

    accepted as browser icon

--- button/popup.js
+++ button/popup.js
@@ -1,4 +1,3 @@
-// content.js
 document.addEventListener('DOMContentLoaded', function () {
     const data = {
         labels: ['Completed', 'Remaining'],
@@ -12,25 +11,21 @@ document.addEventListener('DOMContentLoaded', function () {
     };
 
     const ctx = document.getElementById('pieChart').getContext('2d');
-
     const pieChart = new Chart(ctx, {
         type: 'pie',
         data: data,
         options: {}
     });
 
-    // Add event listener to radio buttons
     const radioButtons = document.querySelectorAll('input[type="radio"]');
     radioButtons.forEach(button => {
         button.addEventListener('change', function () {
-            // Update chart data based on selected job
-			// However, this is currently hardcoded. 
             if (this.value === 'Job 1') {
-                pieChart.data.datasets[0].data = [60, 40]; 
+                pieChart.data.datasets[0].data = [60, 40];
             } else if (this.value === 'Job 2') {
-                pieChart.data.datasets[0].data = [80, 20]; 
+                pieChart.data.datasets[0].data = [80, 20];
             }
-            pieChart.update(); // Update chart
+            pieChart.update();
         });
     });
 });
