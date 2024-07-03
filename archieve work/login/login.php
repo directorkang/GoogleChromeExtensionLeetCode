f5fb1c660f3ec46f7c9bb06173be72c943c7b241
LumWenKang <lumlaoren@gmail.com>
2024-07-04 00:36:39 +0800

    Revert "confuse about file pathing. testing"
    This reverts commit dd037299b9ebc518f6908392d32d3a3621d1ac30.

--- archieve work/login/login.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $userType = $_POST["userType"];
-    // Redirect to the appropriate dashboard based on user type
-    header("Location: " . ($userType === "employee" ? "employee_dashboard.php" : "employer_dashboard.php"));
-    exit();
-}
-?>
